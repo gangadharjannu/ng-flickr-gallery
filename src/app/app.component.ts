945b881dca028ba42ecafd6d2035df0b3ee9e910
gangadharjannu <gangadhar014@gmail.com>
2020-07-20 00:15:44 +0200

    Adds and integrates search box and image grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { map } from 'rxjs/operators';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-flickr-gallery';
+  images$;
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Executed whenever user submits the search.
+   * Make a call to flickr API to get the images list and then form a new url to get the images
+   *
+   * @param searchText - Recieved search text from header component.
+   */
+  onSearch(searchText) {
+    console.log(searchText);
+    this.images$ = this.http.get(`${environment.apiUrl}${searchText}`).pipe(
+      map((response: any) => {
+        console.log(response);
+        return response.photos.photo.map((photoData) => {
+          const url = environment.photoUrl
+            .replace('{farm-id}', photoData.farm)
+            .replace('{server-id}', photoData.server)
+            .replace('{id}', photoData.id)
+            .replace('{secret}', photoData.secret);
+
+          return {
+            url,
+            postedDate: photoData.dateupload,
+            title: photoData.title,
+            ownername: photoData.ownername,
+          };
+        });
+      })
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ImageContainerComponent } from './image-container/image-container.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, ImageContainerComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
