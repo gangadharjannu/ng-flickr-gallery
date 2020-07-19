d53f19913be4c2816afd5246a569e0d78f1a5a62
gangadharjannu <gangadhar014@gmail.com>
2020-07-20 00:12:09 +0200

    Adds image-container component with modal window and edge detection

---
+++ src/app/image-container/image-container.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
+// TypeScript doesn't about cv so it complains about type. Inorder to prevent this we have to delcare a vriable of type any.
+declare var cv: any;
+
+@Component({
+  selector: 'app-image-container',
+  templateUrl: './image-container.component.html',
+  styleUrls: ['./image-container.component.scss'],
+})
+export class ImageContainerComponent implements OnInit {
+  @Input() images;
+  @ViewChild('canvas', { static: true })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  show = false;
+  ctx;
+  title;
+  postedDate;
+  ownerName;
+
+  ngOnInit(): void {
+    // get the native element of canvas and store it in instance variable
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
+
+  /**
+   * Reads the image directly from event.target instead of again download the image (Improvement).
+   * detect the edges and render it on canvas along with image data in a modal window.
+   *
+   * @param event - Image click event
+   * @param image - Image data received from API
+   */
+  detectEdge(event, image) {
+    const src = cv.imread(event.target);
+    const dst = new cv.Mat();
+    cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+    // You can try more different parameters
+    cv.Canny(src, dst, 50, 100, 3, false);
+    cv.imshow(this.canvas.nativeElement, dst);
+    src.delete();
+    dst.delete();
+    this.title = image.title;
+    this.postedDate = new Date(image.postedDate * 1000).toLocaleDateString();
+    this.ownerName = image.ownername;
+    this.showModal();
+  }
+
+  closeModal() {
+    this.show = false;
+  }
+
+  showModal() {
+    this.show = true;
+  }
+}
