8c281f6470e92804be5d74ac8cea8d4877e24c63
gangadharjannu <gangadhar014@gmail.com>
2020-07-20 21:42:22 +0200

    Separates url with query params and adds size to photoUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,16 @@
 export const environment = {
   production: true,
-  apiUrl:
-    'https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=ef1f9d4f8ca80dada31c684364355282&FLickrApi_sig=d7f57fa9e01a6a2d6ccd8597b8d2f86b&nojsoncallback=1&format=json&page=0&per_page=50&content_type=7&extras=owner_name,date_upload&text=',
+  apiUrl: 'https://api.flickr.com/services/rest',
+  method: 'flickr.photos.search',
+  api_key: 'ef1f9d4f8ca80dada31c684364355282',
+  FLickrApi_sig: 'd7f57fa9e01a6a2d6ccd8597b8d2f86b',
+  nojsoncallback: '1',
+  format: 'json',
+  page: '0',
+  per_page: '50',
+  content_type: '7',
+  extras: 'owner_name,date_upload',
+  text: '',
   photoUrl:
-    'https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg',
+    'https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_{size}.jpg',
 };
